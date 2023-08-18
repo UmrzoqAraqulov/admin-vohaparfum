617bd74cd69fe1abb49dfe1a1fa8483165801e45
Umrzoq0323 <araqulovumrzoq0323@gmail.com>
2023-08-18 23:45:24 +0500

    end project

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@
 
 import { getLocalStorage } from "@/const";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const routes = useRouter();
   const token = getLocalStorage();
 
-  if (token) {
-    routes.push("/dashboard");
-  } else {
-    routes.push("/login");
-  }
+  useEffect(() => {
+    if (token) {
+      routes.push("/dashboard");
+    } else {
+      routes.push("/login");
+    }
+  }, [token,routes]);
 }
